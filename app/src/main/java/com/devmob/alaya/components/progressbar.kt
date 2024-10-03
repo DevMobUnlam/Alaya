4b3b15ddfe7ed0cf08552b6c8eef473708700fb0
FlorSantamaria <florenciasantamaria98@gmail.com>
2024-10-03 17:50:23 -0300

    Saco comentarios y redondeo las barras como en el diseño del componente

--- app/src/main/java/com/devmob/alaya/components/progressbar.kt
+++ app/src/main/java/com/devmob/alaya/components/progressbar.kt
@@ -6,44 +6,53 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.devmob.alaya.ui.theme.ColorTertiary
 
 @Composable
 fun SegmentedProgressBar(
     totalSteps: Int,
     currentStep: Int,
-    segmentWidth: Float = 200f, // Ajustado para hacer los indicadores más largos
-    segmentSpacing: Float = 20f, // Ajustado para modificar el espacio entre segmentos
-    filledColor: Color = Color(0xFF7D8DF1),
+    segmentWidth: Float = 600f,
+    segmentSpacing: Float = 20f,
+    filledColor: Color = ColorTertiary,
     unfilledColor: Color = Color(0xFFE0E5F1)
 ) {
-    val totalBarWidth = totalSteps * (segmentWidth + segmentSpacing) - segmentSpacing
+    val totalBarWidth = (segmentWidth - (segmentSpacing * (totalSteps - 1))) / totalSteps
 
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    ) {
     Canvas(
         modifier = Modifier
-            .width(totalBarWidth.dp)
-            .height(20.dp)
+            .width(segmentWidth.dp)
+            .height(8.dp)
+
     ) {
         for (i in 0 until totalSteps) {
-            val startX = i * (segmentWidth + segmentSpacing)
+            val startX = i * (totalBarWidth + segmentSpacing)
 
-            drawRect(
+            drawRoundRect(
                 color = if (i < currentStep) filledColor else unfilledColor,
                 topLeft = Offset(startX, 0f),
-                size = Size(segmentWidth, size.height)
+                size = Size(totalBarWidth, size.height),
+                cornerRadius = CornerRadius(50f, 50f)
             )
-        }
+        }}
     }
 }
 
 @Composable
-fun ProgressBarScreen() {
+fun ProgressBarScreen(modifier: Modifier = Modifier) {
     var currentStep by remember { mutableStateOf(0) }
-    val totalSteps = 6 // Cambia esto según el número de pasos que tengas
+    val totalSteps = 3
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -54,7 +63,6 @@ fun ProgressBarScreen() {
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        // Botones para avanzar y retroceder entre los pasos
         Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
             Button(onClick = { if (currentStep > 0) currentStep-- }) {
                 Text("Anterior")
