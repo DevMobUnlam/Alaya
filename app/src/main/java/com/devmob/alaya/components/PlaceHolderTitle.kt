d211888d17064c94ecc83980c463137aa35c68b6
Facundo <facusosa1607@gmail.com>
2024-10-03 12:26:13 -0300

    Implementacion PlaceHolder y PlaceHolder con titulo

---
+++ app/src/main/java/com/devmob/alaya/components/PlaceHolderTitle.kt
@@ -0,0 +1,97 @@
+package com.devmob.alaya.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Mic
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.devmob.alaya.ui.theme.ColorPrimary
+import com.devmob.alaya.ui.theme.ColorText
+import com.devmob.alaya.ui.theme.ColorWhite
+
+@Composable
+fun PlaceHolderTitle(title: String){
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Text(
+            text = title,
+            fontSize = 30.sp,
+            color = ColorText,
+            modifier = Modifier.padding(horizontal = 90.dp),
+        )
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Card(
+            modifier = Modifier
+                .height(600.dp),
+            colors = CardDefaults.cardColors(containerColor = Color.White),
+            shape = RoundedCornerShape(30.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+            BasicTextField(
+                value = text,
+                onValueChange = { newText -> text = newText },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                textStyle = LocalTextStyle.current.copy(fontSize = 18.sp),
+                decorationBox = { innerTextField ->
+                    if (text.text.isEmpty()) {
+                        Text("Escribe aquí...", color = Color.Gray)
+                    }
+                    innerTextField()
+                }
+            )
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+
+        FloatingActionButton(
+            onClick = {},
+            shape = CircleShape,
+            containerColor = ColorPrimary ,
+            modifier = Modifier
+                .size(80.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Mic,
+                contentDescription = "Agregar",
+                tint = ColorWhite,
+                modifier = Modifier
+                    .size(35.dp)
+            )
+        }
+    }
+}
