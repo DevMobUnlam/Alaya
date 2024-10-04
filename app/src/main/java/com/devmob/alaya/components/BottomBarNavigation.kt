01dc9a4248f951a0ed67d68d77629f4d577c1223
soleecito <soledadrotiroti@gmail.com>
2024-10-03 22:37:55 -0300

    patient icon

--- app/src/main/java/com/devmob/alaya/components/BottomBarNavigation.kt
+++ app/src/main/java/com/devmob/alaya/components/BottomBarNavigation.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
@@ -14,7 +15,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.Bolt
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -28,12 +28,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.devmob.alaya.R
 import com.devmob.alaya.model.IconType
 import com.devmob.alaya.model.ItemMenu
 import com.devmob.alaya.ui.theme.ColorPrimary
@@ -74,8 +77,20 @@ private fun currentRoute(navHostController: NavHostController): String? {
 @Composable
 fun IconMenu(iconType: IconType) {
     when (iconType) {
-        IconType.HOME -> Icon(Icons.Default.Home, contentDescription = "Home", tint = ColorText, modifier = Modifier.size(48.dp))
-        IconType.MENU -> Icon(Icons.Default.Menu, contentDescription = "Menu", tint = ColorText, modifier = Modifier.size(48.dp))
+        IconType.HOME -> Icon(
+            Icons.Default.Home,
+            contentDescription = "Home",
+            tint = ColorText,
+            modifier = Modifier.size(48.dp)
+        )
+
+        IconType.MENU -> Icon(
+            Icons.Default.Menu,
+            contentDescription = "Menu",
+            tint = ColorText,
+            modifier = Modifier.size(48.dp)
+        )
+
         IconType.PROFESSIONAL -> FloatingMiddleButton(iconType)
         IconType.PATIENT -> FloatingMiddleButtonWithAnimation(iconType)
     }
@@ -91,11 +106,11 @@ fun FloatingMiddleButton(iconType: IconType) {
         shape = CircleShape
     ) {
         when (iconType) {
-            IconType.PATIENT -> Icon(
-                Icons.Outlined.Bolt,
+            IconType.PATIENT -> Image(
+                painter = painterResource(id = R.mipmap.ic_patient), // Reemplaza con el ID correcto
                 contentDescription = "manejo de crisis",
-                modifier = Modifier.size(48.dp),
-                tint = ColorWhite
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(48.dp)
             )
 
             IconType.PROFESSIONAL -> Icon(
@@ -148,7 +163,7 @@ fun NavigationBarPreview() {
                 items = listOf(
                     ItemMenu(iconType = IconType.HOME, route = "home"),
                     ItemMenu(iconType = IconType.PATIENT, route = "manejo de crisis"),
-                    ItemMenu(iconType =  IconType.MENU, route =  "menu hamburguesa")
+                    ItemMenu(iconType = IconType.MENU, route = "menu hamburguesa")
                 ), navHostController = navHostController
             )
         }
