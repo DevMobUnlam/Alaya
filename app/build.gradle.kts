9823364e33c7c2d5be5a5015a8ca174de4b973de
FlorSantamaria <florenciasantamaria98@gmail.com>
2024-10-05 16:19:56 -0300

    Borro linea de nav_version en el build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,6 @@ android {
 }
 
 dependencies {
-    val nav_version = "2.7.7"
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
