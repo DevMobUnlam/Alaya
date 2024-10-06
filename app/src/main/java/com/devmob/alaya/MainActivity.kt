daa4bb0c58679afc2f8d3fca799961fd88ecdefe
Nahuel Pachano <nahuelpachano18@gmail.com>
2024-10-06 12:47:15 -0300

    salvado por Sole

--- app/src/main/java/com/devmob/alaya/MainActivity.kt
+++ app/src/main/java/com/devmob/alaya/MainActivity.kt
@@ -7,9 +7,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import com.devmob.alaya.ui.theme.AlayaTheme
-import com.devmob.alaya.components.DateTimePicker
-import com.devmob.alaya.components.ProgressBarScreen
-import com.devmob.alaya.components.SegmentedProgressBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,29 +14,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AlayaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                       name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                MainContent(navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AlayaTheme {
-        Greeting("Android")
-    }
-}
+    }}
