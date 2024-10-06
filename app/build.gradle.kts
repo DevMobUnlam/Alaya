e0aceb4de8ded00e98544472f4f08695a690e54b
soleecito <soledadrotiroti@gmail.com>
2024-10-06 17:02:51 -0300

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,5 +75,5 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.5.0")
     implementation("io.coil-kt:coil-compose:2.1.0")
     implementation(libs.androidx.constraint)
-
+    testImplementation(libs.mockk)
 }
