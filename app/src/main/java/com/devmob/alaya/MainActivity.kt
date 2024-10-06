83c96de11407b78eb912eae51b9236e3fa3c67df
Nahuel Pachano <nahuelpachano18@gmail.com>
2024-10-06 13:05:52 -0300

    Arreglando caos

--- app/src/main/java/com/devmob/alaya/MainActivity.kt
+++ app/src/main/java/com/devmob/alaya/MainActivity.kt
@@ -11,9 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.devmob.alaya.components.FabGroup
 import com.devmob.alaya.ui.theme.AlayaTheme
-import com.devmob.alaya.components.DateTimePicker
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
