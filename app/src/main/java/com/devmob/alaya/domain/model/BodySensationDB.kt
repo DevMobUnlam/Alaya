56018a630c50e5031b28b3e0a82c51d7ecfee368
Bren <rodriguez.brenm@gmail.com>
2024-10-26 22:18:18 -0300

    Add new models for DB and mappers.

---
+++ app/src/main/java/com/devmob/alaya/domain/model/BodySensationDB.kt
@@ -0,0 +1,6 @@
+package com.devmob.alaya.domain.model
+
+data class BodySensationDB(
+    val name: String,
+    val intensity: Intensity
+)
---
+++ app/src/main/java/com/devmob/alaya/domain/model/util/Mappers.kt
@@ -0,0 +1,24 @@
+package com.devmob.alaya.domain.model.util
+
+import com.devmob.alaya.domain.model.BodySensationDB
+import com.devmob.alaya.domain.model.CrisisBodySensation
+import com.devmob.alaya.domain.model.CrisisDetails
+import com.devmob.alaya.domain.model.CrisisDetailsDB
+import com.devmob.alaya.domain.model.CrisisEmotion
+import com.devmob.alaya.domain.model.CrisisEmotionDB
+
+internal fun CrisisDetails.toDB(): CrisisDetailsDB {
+    return CrisisDetailsDB(
+        start = this.crisisTimeDetails.startTime,
+        end = this.crisisTimeDetails.endTime,
+        place = this.placeList.first().name,
+        bodySensations = this.bodySensationList.map { it.toDB() },
+        tools = this.toolList.map { it.name },
+        emotions = this.emotionList.map { it.toDB() },
+        notes = this.notes
+    )
+}
+
+internal fun CrisisEmotion.toDB() = CrisisEmotionDB(this.name, this.intensity)
+
+internal fun CrisisBodySensation.toDB() : BodySensationDB = BodySensationDB(this.name, this.intensity)
