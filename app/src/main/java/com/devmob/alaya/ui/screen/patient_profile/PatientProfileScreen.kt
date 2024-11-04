b353f5472dc6cc517b6aeb27860970d5994d5b66
Lean M <leandro.emuia@gmail.com>
2024-11-03 21:06:50 -0300

    Se agregó scroll a patient profile screen

--- app/src/main/java/com/devmob/alaya/ui/screen/patient_profile/PatientProfileScreen.kt
+++ app/src/main/java/com/devmob/alaya/ui/screen/patient_profile/PatientProfileScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -54,7 +56,9 @@ fun PatientProfileScreen(navController: NavController) {
     }
 
     ConstraintLayout(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         val (image, header, summaryCard, treatmentButton, sessionButton, whatsappButton) = createRefs()
 
