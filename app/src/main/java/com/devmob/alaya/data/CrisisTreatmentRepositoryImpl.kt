300b5508f4bb31434385a7bb5652e4803a1a12d4
Bren <rodriguez.brenm@gmail.com>
2024-11-12 21:06:49 -0300

    Refactor SaveCrisisTreatmentUseCase.

--- app/src/main/java/com/devmob/alaya/data/CrisisTreatmentRepositoryImpl.kt
+++ app/src/main/java/com/devmob/alaya/data/CrisisTreatmentRepositoryImpl.kt
@@ -1,39 +1,20 @@
 package com.devmob.alaya.data
 
-import android.net.Uri
-import android.util.Log
 import com.devmob.alaya.data.mapper.toResponseFirebase
 import com.devmob.alaya.domain.CrisisTreatmentRepository
-import com.devmob.alaya.domain.UploadImageToFirestoreUseCase
-import com.devmob.alaya.domain.model.FirebaseResult
 import com.devmob.alaya.domain.model.OptionTreatment
 import com.devmob.alaya.domain.model.User
 import kotlinx.coroutines.tasks.await
 
 class CrisisTreatmentRepositoryImpl : CrisisTreatmentRepository {
     private val db = FirebaseClient().db
-    private val uploadImage = UploadImageToFirestoreUseCase()
 
-    override suspend fun addCustomTreatment(
+    override suspend fun saveCustomTreatment(
         patientEmail: String,
         treatment: List<OptionTreatment?>
     ) = runCatching {
-        val treatmentList = mutableListOf<OptionTreatment>()
-
-        treatment.forEach { optionTreatment ->
-            optionTreatment?.imageUri?.let { imageUri ->
-                val image = uploadImage(imageUri)
-                if (image != null) {
-                    val updatedTreatment = optionTreatment.copy(imageUri = image.toString())
-                    treatmentList.add(updatedTreatment)
-                } else {
-                    val updatedTreatment = optionTreatment.copy(imageUri = "")
-                    treatmentList.add(updatedTreatment)
-                }
-            }
-        }
         db.collection("users").document(patientEmail)
-            .update("stepCrisis", treatmentList)
+            .update("stepCrisis", treatment)
     }.toResponseFirebase()
 
     override suspend fun getCustomTreatment(patientEmail: String): List<OptionTreatment>? {
--- app/src/main/java/com/devmob/alaya/domain/SaveCrisisTreatmentUseCase.kt
+++ app/src/main/java/com/devmob/alaya/domain/SaveCrisisTreatmentUseCase.kt
@@ -4,14 +4,27 @@ import com.devmob.alaya.data.CrisisTreatmentRepositoryImpl
 import com.devmob.alaya.domain.model.FirebaseResult
 import com.devmob.alaya.domain.model.OptionTreatment
 
-class SaveCrisisTreatmentUseCase {
-    private val CustomTreatmentRepository = CrisisTreatmentRepositoryImpl()
-
+class SaveCrisisTreatmentUseCase(
+    private val customTreatmentRepository: CrisisTreatmentRepositoryImpl = CrisisTreatmentRepositoryImpl(),
+    private val uploadImage: UploadImageToFirestoreUseCase = UploadImageToFirestoreUseCase()
+) {
     suspend operator fun invoke(
         patientEmail: String,
         treatment: List<OptionTreatment?>
     ): FirebaseResult {
-        val result = CustomTreatmentRepository.addCustomTreatment(patientEmail, treatment)
-        return result
+        val treatmentList = mutableListOf<OptionTreatment>()
+        treatment.forEach { optionTreatment ->
+            optionTreatment?.imageUri?.let { imageUri ->
+                val image = uploadImage(imageUri)
+                if (image != null) {
+                    val updatedTreatment = optionTreatment.copy(imageUri = image.toString())
+                    treatmentList.add(updatedTreatment)
+                } else {
+                    val updatedTreatment = optionTreatment.copy(imageUri = "")
+                    treatmentList.add(updatedTreatment)
+                }
+            }
+        }
+        return customTreatmentRepository.saveCustomTreatment(patientEmail, treatment)
     }
 }
