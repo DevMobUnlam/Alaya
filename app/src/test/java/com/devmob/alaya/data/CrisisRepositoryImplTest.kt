353e7b384d4d1330f632b3c102c1000098d19bd5
unknown <maurojose029@gmail.com>
2024-11-16 11:48:55 -0300

    modified test file

--- app/src/test/java/com/devmob/alaya/data/CrisisRepositoryImplTest.kt
+++ app/src/test/java/com/devmob/alaya/data/CrisisRepositoryImplTest.kt
@@ -4,19 +4,25 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.devmob.alaya.domain.CrisisRepository
 import com.devmob.alaya.domain.model.CrisisDetailsDB
 import com.devmob.alaya.domain.model.FirebaseResult
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirestoreRegistrar
+import com.google.firebase.firestore.QuerySnapshot
 import io.mockk.MockKAnnotations
 import io.mockk.clearAllMocks
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import io.mockk.mockkStatic
 import io.mockk.unmockkAll
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
@@ -39,6 +45,9 @@ class CrisisRepositoryImplTest{
 
     @MockK
     private lateinit var successMock: DocumentReference
+
+    @MockK
+    private lateinit var querySnapshotMockK: QuerySnapshot
 
     @MockK
     private lateinit var firebaseAuth: FirebaseAuth
@@ -78,6 +87,7 @@ class CrisisRepositoryImplTest{
 
         val expectedResult = FirebaseResult.Success
 
+
         coEvery{firebaseClient.auth.currentUser?.email?.let {
             firebaseClient
                 .db
@@ -95,7 +105,41 @@ class CrisisRepositoryImplTest{
     }
 
 //    @Test
-//    fun `given registers exits, when getRegisters is called, response is not empty`() = runBlocking {
+//    fun `given registers exist, when getRegisters is called, response is not empty`() = runBlocking {
+//        //GIVEN
+//        val register = CrisisDetailsDB().copy(place = "Casa")
+//
+//        val patientID = "email@test.com"
+//
+//        val documentSnapshotMockK = mockk<DocumentSnapshot>()
+//        val querySnapshotMock = mockk<QuerySnapshot>()
+//        val taskMock = mockk<Task<QuerySnapshot>>()
+//
+//        coEvery { querySnapshotMock.documents } returns mutableListOf(documentSnapshotMockK)
+//        coEvery { taskMock.isSuccessful } returns true
+//        coEvery { taskMock.result } returns querySnapshotMock
+//
+//        coEvery { taskMock.addOnSuccessListener(any()) } returns taskMock
+//
+//        coEvery{db
+//            .collection("users")
+//            .document(patientID)
+//            .collection("crisis_registers")
+//            .orderBy("start",any())
+//            .limit(1)
+//            .get()
+//        } returns taskMock
+//
+//
+//        //WHEN
+//        val result = crisisRepository.getRegisters(patientID){}.first()?.first()
+//
+//        //THEN
+//        Assert.assertEquals(register,result)
+//    }
+
+    //    @Test
+//    fun `given no registers exist, when getRegisters is called, response is empty`() = runBlocking {
 //        //GIVEN
 //        val register = CrisisDetailsDB().copy(place = "Casa")
 //        val patientID = "email@test.com"
@@ -117,5 +161,6 @@ class CrisisRepositoryImplTest{
 //        Assert.assertEquals(register,result)
 //    }
 
+    //TODO GETLASTCRISISDETAILS - UPDATECRISISDETAILS
 
 }
