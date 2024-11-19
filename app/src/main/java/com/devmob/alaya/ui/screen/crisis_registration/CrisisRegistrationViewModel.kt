99ee3c35d59eb5f7379339121f7968347815da6a
Bren <rodriguez.brenm@gmail.com>
2024-11-19 20:43:24 -0300

    Fix: que se seleccionen las herramientas usadas unicamente.

--- app/src/main/java/com/devmob/alaya/ui/screen/crisis_registration/CrisisRegistrationViewModel.kt
+++ app/src/main/java/com/devmob/alaya/ui/screen/crisis_registration/CrisisRegistrationViewModel.kt
@@ -68,7 +68,7 @@ class CrisisRegistrationViewModel(
             _crisisDetails.value = result
 
             if (result != null) {
-                if (result.completed == false) {
+                if (!result.completed) {
                     val startTime = result.start
                     val endTime = result.end
                     if (startTime != null && endTime != null) {
@@ -234,6 +234,17 @@ class CrisisRegistrationViewModel(
         updateStateEmotionList(currentState, updatedEmotionList)
     }
 
+    fun selectCrisisTool(tool: CrisisTool) {
+        val currentState = _screenState.value ?: return
+        val updatedToolList =
+            currentState.crisisDetails.toolList.toMutableList().apply {
+                if (!any { it.name == tool.name }) {
+                    add(tool)
+                }
+            }
+        updateStateToolList(currentState, updatedToolList)
+    }
+
     fun updateIntensityEmotion(crisisEmotion: CrisisEmotion, intensity: Intensity) {
         val currentState = _screenState.value ?: return
         val updatedEmotionList =
@@ -256,6 +267,18 @@ class CrisisRegistrationViewModel(
             )
         )
     }
+
+    private fun updateStateToolList(
+        currentState: CrisisRegistrationScreenState,
+        updatedToolList: MutableList<CrisisTool>
+    ) {
+        _screenState.value = currentState.copy(
+            crisisDetails = currentState.crisisDetails.copy(
+                toolList = updatedToolList
+            )
+        )
+    }
+
 
     fun updateCrisisEmotion(emotion: CrisisEmotion) {
         val currentState = _screenState.value ?: return
